6aa729837d0ee8937653fabb467572483864dac1
lsamano <leizl.samano@gmail.com>
2020-04-11 11:09:23 -0400

    added styling and QuestionContainer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import QuestionContainer from './QuestionContainer';
 
 function App() {
+  const [loaded, setLoaded] = useState(false)
+  const [ questionsArray, setQuestionsArray ] = useState([])
+  const [ currentIndex, setCurrentIndex ] = useState(0)
+
+  useEffect(() => {
+    // fetch and set questions
+    axios.get("http://localhost:3000/questions")
+    .then(res => {
+      console.log(res);
+      setQuestionsArray(res.data);
+      setLoaded(true);
+    })
+  },  [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>QUIZL</h1>
+        <span>Challenge Yourself.</span>
       </header>
+      <main>
+        {
+          loaded && <QuestionContainer question={questionsArray[0]}/>
+        }
+      </main>
     </div>
   );
 }
---
+++ src/QuestionContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const QuestionContainer = ({ question }) => (
+  <div className="fade-in">
+    <h2>{question.text}</h2>
+    make radio dials
+  </div>
+);
+
+export default QuestionContainer;
