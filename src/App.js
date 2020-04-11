aa024ab5d29eae490c3ec82f5eca6076779ab734
lsamano <leizl.samano@gmail.com>
2020-04-11 19:31:55 -0400

    quiz advances

--- src/App.js
+++ src/App.js
@@ -7,16 +7,29 @@ import QuestionContainer from './QuestionContainer';
 function App() {
   const [loaded, setLoaded] = useState(false)
   const [ questionsArray, setQuestionsArray ] = useState([])
-  const [ currentIndex, setCurrentIndex ] = useState(0)
+  const [ currentIndex, setCurrentIndex ] = useState(-1)
 
   useEffect(() => {
     // fetch and set questions
     axios.get("http://localhost:3000/questions")
     .then(res => {
       setQuestionsArray(res.data);
+      setCurrentIndex(0);
       setLoaded(true);
     })
   },  [])
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // record answer
+    if (currentIndex + 1 === questionsArray.length) {
+      // finish the quiz
+      alert("Quiz is done!")
+    } else {
+      // go to next question
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
 
   return (
     <div className="App">
@@ -27,8 +40,11 @@ function App() {
         {
           loaded &&
           <QuestionContainer
+            key={currentIndex}
             question={questionsArray[currentIndex]}
             questionNumber={currentIndex + 1}
+            amountOfQuestions={questionsArray.length}
+            handleSubmit={handleSubmit}
             />
         }
       </main>
--- src/QuestionContainer.jsx
+++ src/QuestionContainer.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Choice from './Choice';
 
-const QuestionContainer = ({ question, questionNumber }) => {
+const QuestionContainer = ({ question, questionNumber, amountOfQuestions, handleSubmit }) => {
   const formattedChoices = question.choices.map(
     (text, index) => <Choice text={text} key={index}/>
 )
+
+  const calculateProgress = `${questionNumber}/${amountOfQuestions}`
 
   return (
     <form className="fade-in">
       <h2>{`${questionNumber}. ${question.text}`}</h2>
       { formattedChoices }
-      <input type='submit' value="Next Question" />
-      <input type='button' value="Next Question" />
+      <input type='button' value="Back" />
+      { calculateProgress }
+      <input type='submit' value="Next" onClick={handleSubmit} />
     </form>
   )
 };
