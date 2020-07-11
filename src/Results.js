b8ca9984c0dd752663cc51cad6127996bec3ce57
lsamano <leizl.samano@gmail.com>
2020-07-11 11:01:32 -0400

    minor cleanup

--- src/Results.js
+++ src/Results.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const Results = ({ score, numberOfQuestions, gradedArray }) => {
   const showAnswers = () => {
-    return gradedArray.map((answer, index) => <li key={index}>{ answer ? "CORRECT" : "X" }</li>)
+    return gradedArray.map(
+      (answer, index) => <li key={index}>{ answer ? "CORRECT" : "X" }</li>
+    )
   }
 
   return (
     <>
-      Results Screen
-      Score: { score } / { numberOfQuestions }
-      <br />
-      {score/numberOfQuestions * 100}%
+      <h2>
+        Results
+      </h2>
+      <p>
+        Score: { score } / { numberOfQuestions }
+      </p>
+      <p>
+        {score/numberOfQuestions * 100}%
+      </p>
       <ol>
       { showAnswers() }
       </ol>
