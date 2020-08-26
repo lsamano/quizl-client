95dd7ef30db69e8dbb459e166d346a73af9279c3
lsamano <leizl.samano@gmail.com>
2020-08-26 16:08:57 -0400

    changed styling

--- src/ChooseQuiz.js
+++ src/ChooseQuiz.js
@@ -3,14 +3,16 @@ import React from 'react';
 const ChooseQuiz = ({ setIndex, quizzes }) => {
   const listQuizzes = () => {
     return quizzes.map((quiz, index) => (
-      <div key={quiz._id} onClick={event => setIndex(index)}>{quiz.title}</div>
+      <li style={{listStyleType: "none", }} key={quiz._id} onClick={event => setIndex(index)}>{quiz.title}</li>
     ))
   }
-  
+
   return (
     <div>
       <h2>Choose a Quiz:</h2>
-    { listQuizzes() }
+      <ul>
+        { listQuizzes() }
+      </ul>
     </div>
   )
 }
